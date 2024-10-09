f89c0bf837395446e5ec4fa5038072677d921d0d
Francielle Dellamora <francielle@dellamora.dev>
2024-10-09 09:55:50 -0300

    Update replace-word.sh

--- replace-word.sh
+++ replace-word.sh
@@ -1,5 +1,5 @@
 find . -type f -name "*.agda" | while read -r file; do
     # Use sed to replace the arrows in each .agda file
-    sed -i 's/→/->/g;' "$file"
+    sed -i '''s/→/->/g;' "$file"
     echo "Processed: $file"
 done
